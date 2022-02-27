ec1232933c71c21a044214d6dd40e53c47525451
swisstackle <alainsch@buffalo.edu>
2022-02-26 21:27:49 -0500

    Removed all string storage from contract, because strings are to expensive to be stored in the blockchain

--- Players.sol
+++ Players.sol
@@ -4,30 +4,20 @@ pragma solidity ^0.8.7;
 import "hardhat/console.sol";
 contract Players{
      address payable chairperson;
-      mapping (address=>player) public players;
-      mapping (string=>address payable) public playerToAdress;
+      mapping (address=>uint) public players; // maps player to its balance
       mapping(address =>uint) member;
-      struct player{ 
-        uint balance; // deposit for payment settlement
-        string name;
-        service[] services;
-        }
-
-        struct service{
-            string name;
-            string description;
-            uint price;
-        }
+  
 
       modifier onlyMember{ 
          require(member[msg.sender]==1);
             _;
       }
-      modifier beYourSelf (string memory _name){
-         address payable _address = playerToAdress[_name];
-         require(msg.sender == _address);
-         _;
+      // checks whether the address that the sender wants to send money to is also a member
+      modifier onlyMemberTo(address payable toAddress){ 
+         require(member[toAddress]==1);
+            _;
       }
+   
       modifier beYourSelfViaAddress (address _ad){
          require(msg.sender == _ad);
          _;
@@ -35,55 +25,53 @@ contract Players{
     constructor () public payable { 
          chairperson= payable(msg.sender);
          
-         players[chairperson].balance = msg.sender.balance;
-         players[chairperson].name = "Chairperson";
-         playerToAdress["Chairperson"] = chairperson;
+         players[chairperson] = msg.sender.balance;
         // chairperson.transfer(msg.value);
          console.log("System has been initiated with chairperson: ", chairperson, " with balance ", msg.sender.balance);
         }
-     function register (string memory name) public payable{ 
-        address payable newPlayer = payable(msg.sender);
-        players[newPlayer].name =name;
-        
-        players[newPlayer].balance = msg.sender.balance;
-        playerToAdress[name] = newPlayer;
+     function register (address payable newPlayer) public payable{ 
+        players[newPlayer] = msg.sender.balance;
         member[newPlayer] = 1;
      }
-     function register_service(service memory s, string memory playerName) onlyMember beYourSelf(playerName) public{
-        address ad = playerToAdress[playerName];
-        players[ad].services.push(s);
-        console.log("Address ", ad, " registred the service called ", s.name);
+   //   function register_service(service memory s, string memory playerName) onlyMember beYourSelf(playerName) public{
+   //      address ad = playerToAdress[playerName];
+   //      players[ad].services.push(s);
+   //      console.log("Address ", ad, " registred the service called ", s.name);
+   //   }
+     function register_service() onlyMember public{
+        // We will only verify whether the sender address can register a service. Only modifier onlyMember() is necessary for that
      }
-     function register_service_by_ad(service memory s, address from) onlyMember beYourSelfViaAddress(from) public{
-        players[from].services.push(s);
-        console.log("Address ", from, " registred the service called ", s.name);
+     function unRegisterService() onlyMember public {
+         //same
      }
 
-     function sendMoney(address payable toAddress) onlyMember payable public {
-         address fromAddress=msg.sender;
+      function editService() onlyMember public {
+         //same
+      }
+      function rateService() onlyMember public {
+         //same
+         
+      }
+      
+     function sendMoney(address payable toAddress) onlyMember onlyMemberTo(toAddress) payable public {
+         address fromAddress = msg.sender;
          uint amt = msg.value;
-         players[toAddress].balance = players[toAddress].balance + amt;
-         players[fromAddress].balance = players[fromAddress].balance - amt;
+         players[toAddress] = players[toAddress] + amt;
+         players[fromAddress] = players[fromAddress] - amt;
          toAddress.transfer(amt); 
      }
-     function sendMoney(string memory toName) onlyMember payable public {
-        uint amt = msg.value;
-        address payable toAddress = playerToAdress[toName];
-         address fromAddress=msg.sender;
-         players[toAddress].balance = players[toAddress].balance + amt;
-         players[fromAddress].balance = players[fromAddress].balance - amt;
-         toAddress.transfer(amt); 
-     }
+   //   function sendMoney(string memory toName) onlyMember payable public {
+   //      uint amt = msg.value;
+   //      address payable toAddress = playerToAdress[toName];
+   //       address fromAddress=msg.sender;
+   //       players[toAddress].balance = players[toAddress].balance + amt;
+   //       players[fromAddress].balance = players[fromAddress].balance - amt;
+   //       toAddress.transfer(amt); 
+   //   }
 
-    function print(string memory name) public view {
-        address a = playerToAdress[name];
-        player memory p = players[a];
-
-        console.log("Player ",name, " has a balance of ", p.balance);
-         console.log(" and offers the services: ");
-         for (uint i = 0; i < p.services.length; i++) {
-            console.log(p.services[i].name);
-         }
+    function print(address a) public view {
+        uint p = players[a];
+        console.log("Player ",a, " has a balance of ", p);
     }
     
 }
