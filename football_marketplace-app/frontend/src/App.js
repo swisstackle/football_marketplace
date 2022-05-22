07ed264953cd583557b97388854bcaceda7c7984
Alain Schaerer <alainsch@buffalo.edu>
2022-05-23 01:51:05 +0200

    #7 Created seperate file for register form

--- football_marketplace-app/frontend/src/App.js
+++ football_marketplace-app/frontend/src/App.js
@@ -8,7 +8,7 @@ import Video from "./Video";
 import Store from "./Store";
 import RequestRegisterService from "./RequestRegisterService";
 import CoachesBackendDom from './CoachesBackendDom';
-
+import RegisterFormDom from './Register';
 
 import {
     Navbar,
@@ -585,70 +585,6 @@ const NavDom = (props) => {
     );
 }
 
-const RegisterFormDom = (props) => {
-    const [isRegistered, setIsRegistered] = useState();
-    useEffect(() => {
-
-        async function load() {
-            console.log(props.contract);
-            const isReg = await props.contract.methods.isRegistered(props.account).call();
-            setIsRegistered(isReg);
-        }
-
-        load();
-    }, [])
-    if (!isRegistered) {
-        return (
-            <Container className="px-5 my-5">
-                <Row className="justify-content-center">
-
-                    <Col className="col-lg-8">
-                        <Card className="border-0 rounded-3 shadow-lg p-5">
-
-                            <div className="card-body p-4">
-                                <center>
-
-                                    <div className="h1 fw-light">Register Yourself</div>
-                                    <p className="mb-4 text-muted">You don't need a password because blockchain
-                                        technology takes care of verifying.</p>
-
-                                </center>
-                            </div>
-
-                            <form className="form-floating">
-                                <center>
-
-                                    <label htmlFor={"username"} className={"mb-2"}>Username</label>
-                                    <input className={"form-control mb-2"} id={"username"} name={"username"}
-                                           placeholder="Username"
-                                    />
-
-                                </center>
-
-                                <div className="d-grid">
-                                    <a href={"#"} onClick={() => registerPlayer(props.contract, props.account)}
-                                       className={"btn btn-primary btn-lg"}>Register</a>
-                                </div>
-
-                            </form>
-                        </Card>
-
-                    </Col>
-
-                </Row></Container>
-
-
-        );
-    } else {
-        return (<Stack className="justify-content-center align-items-center">
-                <h3 className="display-3">You are already registered.</h3>
-            </Stack>
-
-        );
-    }
-
-}
-
 const WalletDom = (props) => {
     const _contract = props.contract;
     const _account = props.account;
@@ -738,7 +674,7 @@ export async function admitService(address, name, description, price) {
     await $.get('admitservice?address=' + address + '&name=' + name + '&description=' + description + '&price=' + price);
 }
 
-function registerPlayer(contract, account) {
+export function registerPlayer(contract, account) {
     console.log(contract.address);
     contract.methods.register().send({from: account}).then(function (receipt) {
         if (receipt) {
