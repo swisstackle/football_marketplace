fb941e675adc395706b42b3292aa9e892ec8793d
Alain Schaerer <alainsch@buffalo.edu>
2022-05-23 03:00:06 +0200

    #14 Encoded name in deleteService()

--- football_marketplace-app/src/js/dbqueries.js
+++ football_marketplace-app/src/js/dbqueries.js
@@ -157,6 +157,8 @@ const deleteServiceRequest = (address,service_name)=>{
     })
 }
 const deleteService = (address, name)=>{
+    //TODO: Check if address does not contain malicious code https://github.com/swisstackle/football_marketplace/issues/34
+    name = encodeURIComponent(name);
     client.query('DELETE FROM services WHERE service_name=$1 AND address=$2',[name, address],(error) =>{
         if(error){
             throw error;
