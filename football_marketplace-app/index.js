89af9e59080509efdebbbf3d9741cc42f0e3add8
Alain Schaerer <alainsch@buffalo.edu>
2022-05-29 23:20:27 +0200

    Update index.js

--- football_marketplace-app/index.js
+++ football_marketplace-app/index.js
@@ -104,6 +104,6 @@ app.get('/servicedone', function(req, res){
 
 app.get('/', async function(req, res){
     res.sendFile(
-        fs.jo(__dirname, "/frontend/build/index.html")
+        path.jo(__dirname, "/frontend/build/index.html")
     );
 });
