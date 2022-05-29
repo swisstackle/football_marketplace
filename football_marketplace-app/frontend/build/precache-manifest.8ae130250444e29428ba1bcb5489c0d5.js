890ed1a7f276475882caba90eb72dda36b7bcd5c
Alain Schaerer <alainsch@buffalo.edu>
2022-05-29 22:32:52 +0200

    productiondirectory

---
+++ football_marketplace-app/frontend/build/precache-manifest.8ae130250444e29428ba1bcb5489c0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5686a774a548e2721bdf",
+    "url": "/static/css/main.4a3b3169.chunk.css"
+  },
+  {
+    "revision": "5686a774a548e2721bdf",
+    "url": "/static/js/main.5686a774.chunk.js"
+  },
+  {
+    "revision": "9cda04ab7e1ca8fe1492",
+    "url": "/static/js/1.9cda04ab.chunk.js"
+  },
+  {
+    "revision": "eca38d1a4f2613cf1fb3",
+    "url": "/static/css/2.de145ee1.chunk.css"
+  },
+  {
+    "revision": "eca38d1a4f2613cf1fb3",
+    "url": "/static/js/2.eca38d1a.chunk.js"
+  },
+  {
+    "revision": "2a3795fee26f86569544",
+    "url": "/static/js/runtime~main.2a3795fe.js"
+  },
+  {
+    "revision": "c3dbc1b571616361becd182d2c4969ad",
+    "url": "/index.html"
+  }
+];
